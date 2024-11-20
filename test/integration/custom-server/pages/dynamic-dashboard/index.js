59cdc95ada7e7af9330c9ccdf6ac7ff3f944d5bb
Janka Uryga <lolzatu2@gmail.com>
2024-11-20 21:24:17 +0100

    test: add tests for legacy NextCustomServer methods

--- test/integration/custom-server/pages/dynamic-dashboard/index.js
+++ test/integration/custom-server/pages/dynamic-dashboard/index.js
@@ -1,11 +1,16 @@
 import { useRouter } from 'next/router'
 
-export default () => {
+// NOTE: we want this page to be dynamic, otherwise the HTML won't contain search params
+export async function getServerSideProps() {
+  return { props: {} }
+}
+
+export default function Page() {
   const router = useRouter()
   const searchParam = router.query.q
   return (
     <p>
-      made it to dashboard
+      made it to dynamic dashboard
       {!!searchParam && (
         <>
           <br />
